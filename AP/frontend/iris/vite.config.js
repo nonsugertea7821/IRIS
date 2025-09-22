2ba7ba350e0ca536cd5c9740367d5f8790cc7665
nonsugertea7821 <nonsugertea7821@gmail.com>
2025-09-22 22:02:34 +0900

    [共通]最新版はHMRにコミュニティで報告されている問題があるようなのでviteに変更

---
+++ AP/frontend/iris/vite.config.js
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    host: 'localhost',
+    port: 3000,
+    open: true,
+  },
+});
