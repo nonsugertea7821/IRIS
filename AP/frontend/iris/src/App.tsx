ff3854fd116a635175f3381d7d448e159da054fe
nonsugertea7821 <nonsugertea7821@gmail.com>
2025-09-22 17:50:15 +0900

    [共通]webpackを直接使うように変更 react-scriptsへの依存を解消
    ※注意
    まだLive-Reloadが使用できないため、ひとまずは下記の通りビルド→起動の順で作業する
    
    1.npm run build
    2.npm start

--- AP/frontend/iris/src/App.tsx
+++ AP/frontend/iris/src/App.tsx
@@ -1,8 +1,8 @@
-// src/App.tsx
+import { JSX } from 'react';
 import { RecoilRoot } from 'recoil';
 import AppRouter from './store/route/AppRouter';
 
-export default function App() {
+export default function App():JSX.Element {
   return (
     <RecoilRoot>
       <AppRouter />
